8978023aa91a117b7243655f258323ff1c457af5
Kaustubh Karthik <kaustubhkarthik@gmail.com>
2023-04-17 20:30:41 +0100

    starting to increase volume

--- beat_playing_algorithm.py
+++ beat_playing_algorithm.py
@@ -2,14 +2,22 @@ from pydub import AudioSegment
 import simpleaudio as sa
 from pygame.time import get_ticks, Clock
 import numpy as np
+import wave
+
+with wave.open("pasoori.wav", "rb") as wav_obj:
+  # Getting metadata of wave audio
+  sample_freq = wav_obj.getframerate()
+  n_samples = wav_obj.getnframes()
+  n_channels = wav_obj.getnchannels()
+  signal_wave = wav_obj.readframes(n_samples)
+
+for x in signal_wave:
+    x += 1000
 
 note_times = np.genfromtxt("turning_points.txt", delimiter = ", ")
 
-print(note_times*1000)
-
 song_obj = sa.WaveObject.from_wave_file("pasoori.wav")
 bonk_obj = sa.WaveObject.from_wave_file("bonk_sound.wav")
-
 
 play_obj = song_obj.play()
 
