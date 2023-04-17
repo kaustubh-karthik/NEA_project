3e50ddc86167a16a893f13956c1415553d91d1aa
Kaustubh Karthik <kaustubhkarthik@gmail.com>
2023-04-17 14:07:10 +0100

    sprites loading

--- main_game.py
+++ main_game.py
@@ -7,7 +7,7 @@ from random import randint
 pygame.init()
 clock = pygame.time.Clock()
 
-# Main Window
+# Main Window setup
 screen_width = 1280
 screen_height = 960
 screen = pygame.display.set_mode((screen_width,screen_height))
@@ -25,18 +25,19 @@ class SpawnPos(IntEnum):
 
 class Note(pygame.sprite.Sprite):
 
-    note_group = pygame.sprite.Group
+    note_group = pygame.sprite.Group()
     spawn_positions = [SpawnPos.ln0, SpawnPos.ln1, SpawnPos.ln2, SpawnPos.ln3, SpawnPos.ln4]
     note_width = lane_size
     note_height = screen_height//10
-    base_surface = pygame.Surface((screen_width, screen_width))
-    base_surface.fill((255, 255, 255))
+#
 
     def __init__(self) -> None:
-        
+        super().__init__()
         random_spawn = Note.get_random_spawn()
-        self.note_rect = pygame.Rect(random_spawn, 0, Note.note_width, Note.note_height)
-        Note.add_to_group(self.note_rect)
+        self.rect = pygame.Rect(random_spawn, 0, Note.note_width, Note.note_height)
+        self.image = pygame.Surface((Note.note_width, Note.note_height))
+        self.image.fill((255, 255, 255))
+        Note.add_to_group(self)
     
     def get_random_spawn() -> SpawnPos:
         return Note.spawn_positions[randint(0, 4)]
@@ -45,17 +46,18 @@ class Note(pygame.sprite.Sprite):
         Note.note_group.add(note_rect)
     
     def draw_notes():
-        Note.note_group.draw(Note.base_surface)
+        Note.note_group.draw(screen)
         
     def generate_notes(num_notes):
         for _ in range(num_notes):
-            Note.add_to_group(Note())
-        print(Note.note_group)
+            Note()
+        print(Note.note_group.sprites())
         
-def run():
-    Note.generate_notes(5)
-    Note.draw_notes()
-    screen.update()
-    
-run()
+
+Note.generate_notes(2)
+Note.draw_notes()
+
+while True:
+    pygame.display.update()
+
     
