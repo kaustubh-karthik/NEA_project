41f4d77fe391318c3839c25d613ab8c65df7c855
Kaustubh Karthik <kaustubhkarthik@gmail.com>
2023-04-18 13:47:36 +0100

    saving a record of tests to folder

--- beat_finding_algorithm.py
+++ beat_finding_algorithm.py
@@ -13,12 +13,17 @@ with wave.open(wav_file_name + ".wav", "rb") as wav_obj:
   n_channels = wav_obj.getnchannels()
   signal_wave = wav_obj.readframes(n_samples)
 
+filt_num = sample_freq # Higher value increases smoothing
+coeff_keep = 100 # Increasing this increases the level of detail in the smoothed signal
+decimal_places = 1 # Higher value reduces amount of zeroes
+rounding_diff = 1 # Higher values increase the amount of duplicates
+
+
 # Converts a buffer(byte) object to an Int16 numpy array
 signal_array = abs(np.frombuffer(signal_wave, dtype=np.int16))
 signal_length = n_samples/sample_freq # In seconds
 
 # Smoothes the wave signal to allow for easier data analysis
-filt_num = sample_freq # Higher value increases smoothing
 filt = np.ones(filt_num)/filt_num # Creates an array of length filt num, with values of 1/filt num
 # Calculates a moving average over filt_num values
 signal_smoothed = scipy.signal.fftconvolve(signal_array, filt, mode="same")
@@ -31,7 +36,6 @@ times = np.linspace(0, signal_length, num=n_samples)
 signal_fft = np.fft.fft(signal_smoothed)
 
 # Keeps only some of the coefficients
-coeff_keep = 100 # Increasing this increases the level of detail in the smoothed signal
 signal_stripped_fft = signal_fft
 signal_stripped_fft[100:] = 0
 
@@ -47,14 +51,12 @@ half_diff = (times_len - wave_len)//2
 smoothed_gradient = np.gradient(conv_smoothed, times).real
 # Turning points are where gradient is equal to 0
 # Need to round as computer will never be accurate enough to know when it is exactly equal to 0
-decimal_places = 1 # Higher value reduces amount of zeroes
 turning_points = np.where(np.round(smoothed_gradient, decimal_places) == 0)[0]
 turning_points_seconds = turning_points/sample_freq # Calculating signal length
 
 # rounding all numbers to 1 decimal place, any duplicates will automatically be deleted when converting to a set
 # Conversion back to list allows for sorting the values
 # Conversion back to array allows for easier writing to file
-rounding_diff = 1 # Higher values increase the amount of duplicates
 distinct_tps = np.asarray(sorted(list(set(np.round(turning_points_seconds, rounding_diff)))))
 
 # Writing all values to a file, separated by ", "
