410730aedf90c9bfc5d122a26eb8247bfcad2700
Kaustubh Karthik <kaustubhkarthik@gmail.com>
2023-04-18 12:32:51 +0100

    added comments to all files

--- beat_playing_algorithm.py
+++ beat_playing_algorithm.py
@@ -12,20 +12,27 @@ with wave.open("bonk_sound.wav", "rb") as wav_obj:
   sample_width = wav_obj.getsampwidth()
   signal_wave = wav_obj.readframes(n_samples)
 
-
+# Converting byte buffer array to numpy array of int16
 signal_wave = abs(np.frombuffer(signal_wave, dtype=np.int16))
+# Increasing volume
 signal_wave = signal_wave * 100
 
+# Generating array of times from the text file
 note_times = np.genfromtxt("turning_points.txt", delimiter = ", ")
 
+# Initialising sound objects
 song_obj = sa.WaveObject.from_wave_file("pasoori.wav")
+# Creating a playable function to play the bonk sound
 bonk_obj = lambda: sa.play_buffer(signal_wave, n_channels, sample_width, sample_freq)
 
+# Starting playback of song
 play_obj = song_obj.play()
 
 while True:
+    # Initialising clock and ticks
     Clock().tick()
-    #print(f"get ticks is: {get_ticks()}")
+    
+    # Checking if the time(in ms) is a time in the note times
     if get_ticks() in note_times*1000:
         bonk_obj()
-        print("yeah", get_ticks())
+        print("yeah", get_ticks()) # debug statement
