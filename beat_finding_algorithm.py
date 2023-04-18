ab1a9d28d78ad5e4e215b4445ad20c71bba5caa1
Kaustubh Karthik <kaustubhkarthik@gmail.com>
2023-04-18 12:32:51 +0100

    added comments to all files

--- beat_finding_algorithm.py
+++ beat_finding_algorithm.py
@@ -12,44 +12,60 @@ with wave.open(wav_file_name + ".wav", "rb") as wav_obj:
   n_samples = wav_obj.getnframes()
   n_channels = wav_obj.getnchannels()
   signal_wave = wav_obj.readframes(n_samples)
-print(n_samples, sample_freq)
 
-# Converts a buffer(byte) object to an Int16 array
+# Converts a buffer(byte) object to an Int16 numpy array
 signal_array = abs(np.frombuffer(signal_wave, dtype=np.int16))
 signal_length = n_samples/sample_freq # In seconds
 
 # Smoothes the wave signal to allow for easier data analysis
 filt_num = sample_freq
-filt = np.ones(filt_num)/filt_num
+filt = np.ones(filt_num)/filt_num # Creates an array of length filt num, with values of 1/filt num
+# Calculates a moving average over filt_num values
 signal_smoothed = scipy.signal.fftconvolve(signal_array, filt, mode="same")
 
 # Creates an array corresponding to the time of each audio sample
+# Creates array of length n_samples, with values going from 0 to signal_length
 times = np.linspace(0, signal_length, num=n_samples)
 
 # Creates a Fast Fourier Transform of the smoothed signal array
 signal_fft = np.fft.fft(signal_smoothed)
 
+# Keeps only some of the coefficients
+coeff_keep = 100 # Increasing this increases the level of detail in the smoothed signal
 signal_stripped_fft = signal_fft
 signal_stripped_fft[100:] = 0
 
-fft_smoothed = np.fft.ifft(signal_stripped_fft)
+fft_smoothed = np.fft.ifft(signal_stripped_fft) # Converts the array of coefficients back to an array of y coordinates
 conv_smoothed = scipy.signal.fftconvolve(fft_smoothed, filt, mode="same") # secondary smoothing
 
+# Calculating difference in length caused by moving average calculation
 times_len = len(times)
 wave_len = len(conv_smoothed)
 half_diff = (times_len - wave_len)//2
 
 # Calculating gradients and turning points
-smoothed_gradient = np.gradient(conv_smoothed, times[half_diff: -half_diff]).real
-turning_points = np.where(np.round(smoothed_gradient, 2) == 0)[0]
-with open("turning_points.txt", 'w') as tps:
-  turning_points_seconds = turning_points/sample_freq
-  turning_points_seconds.tofile(tps, sep=', ')
+smoothed_gradient = np.gradient(conv_smoothed, times).real
+# Turning points are where gradient is equal to 0
+# Need to round as computer will never be accurate enough to know when it is exactly equal to 0
+turning_points = np.where(np.round(smoothed_gradient, 1) == 0)[0]
+turning_points_seconds = turning_points/sample_freq # Calculating signal length
 
+# rounding all numbers to 1 decimal place, any duplicates will automatically be deleted when converting to a set
+# Conversion back to list allows for sorting the values
+# Conversion back to array allows for easier writing to file
+distinct_tps = np.asarray(sorted(list(set(np.round(turning_points_seconds, 1)))))
+
+# Writing all values to a file, separated by ", "
+with open("turning_points.txt", 'w') as tps:
+  distinct_tps.tofile(tps, sep=', ')
+
+# Plotting graphs for debugging
 plt.figure(figsize=(20,5))
-plt.plot(times[half_diff: -half_diff], fft_smoothed)
-plt.plot(turning_points/sample_freq, np.zeros(len(turning_points)), '.')
-plt.plot(times[half_diff: -half_diff], smoothed_gradient, color="r")
+plt.plot(times, fft_smoothed)
+# Creating a dotted graph with dots at zero, and x coordinates at the turing points
+plt.plot(distinct_tps, np.zeros(len(distinct_tps)), '.')
+plt.plot(times, smoothed_gradient, color="r")
 plt.show()
-# Prints the times(in seconds) of turning points
-print("Turning points: ", turning_points/sample_freq)
+
+# Prints the times(in seconds) of distinct turning points - for debugging
+print("Turning points: ", distinct_tps)
--- main_game.py
+++ main_game.py
@@ -15,9 +15,11 @@ screen_height = 1024
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption('NEA Rhythm Game')
 
+# Initialising lanes
 lanes = 5
 lane_size = screen_width//lanes
 
+# Enum to store the x positions of lanes
 class SpawnPos(IntEnum):
     ln0 = 0
     ln1 = lane_size
@@ -25,60 +27,80 @@ class SpawnPos(IntEnum):
     ln3 = lane_size*3
     ln4 = lane_size*4
 
+# Main class
 class Note(pygame.sprite.Sprite):
 
-    note_group = pygame.sprite.Group()
+    # Initialising class variables
+    note_group = pygame.sprite.Group() # Contains all the note objects
+    # Array of spawn positions allows me to randomly select a spawn position
     spawn_positions = [SpawnPos.ln0, SpawnPos.ln1, SpawnPos.ln2, SpawnPos.ln3, SpawnPos.ln4]
+    
+    # Note calculations
     note_width = lane_size
     note_height = screen_height//10
-    note_times = np.genfromtxt("turning_points.txt", delimiter = ", ")
-    bg_image = pygame.image.load("notes_falling.jpg")
-    print(note_times)
     
+    # Generating list of times for notes to spawn from a txt file
+    note_times = np.genfromtxt("turning_points.txt", delimiter = ", ")
+    bg_image = pygame.image.load("notes_falling.jpg") # Loading bg image
 
     def __init__(self) -> None:
+        # Call to super class to initialise this Note instance as a pygame sprite instance
         super().__init__()
+        
+        # Overriding super() class variables
         random_spawn = Note.get_random_spawn()
+        # Base rect
         self.rect = pygame.Rect(random_spawn, 0, Note.note_width, Note.note_height)
+        
+        # Surface to draw rect on
         self.image = pygame.Surface((Note.note_width, Note.note_height))
-        self.image.fill((255, 255, 255))
-        Note.add_to_group(self)
+        self.image.fill((255, 255, 255)) # Adding color to image(white)
+        Note.add_to_group(self) # Adding to sprite group
     
+    # Randomly selects an array value
     def get_random_spawn() -> SpawnPos:
         return Note.spawn_positions[randint(0, 4)]
     
+    # Adds to group
     def add_to_group(note_rect):
         Note.note_group.add(note_rect)
     
+    # Draws notes to the screen
     def draw_notes():
         Note.note_group.draw(screen)
-        
+    
+    # Generates any number of notes at random positions
     def generate_notes(num_notes):
         for _ in range(num_notes):
             Note()
-        #print(Note.note_group.sprites())
-        
+
+    # Generates a note at the correct point(needs to be called in main game loop)        
     def generate_timed_notes(clock_time):
+        # Checks if the game time(ms) is equal to any time in the note_times array
         if clock_time in Note.note_times*1000:
             Note.generate_notes(1)
-            #print(clock_time)
-    
+
+    # Makes every note in the sprite group move down at a consistant speed    
     def note_movement():
+        speed = 1        
+
+        # Iterates through the sprite group and adds a fixed speed to their y value
         for sprite in Note.note_group.sprites():
-            sprite.rect.y += 1
-        Note.note_group.update()
+            sprite.rect.y += speed
+        Note.note_group.update() # Updates all sprites in group
     
+    # Blits the background image onto the screen at coords (0,0) - top left
     def render_background():
         screen.blit(Note.bg_image, (0, 0))
         
-
+# Starting playback of song
 wave_obj = sa.WaveObject.from_wave_file("pasoori.wav")
 wave_obj.play()
 
 
+'''---------------Main game loop------------------'''
 while True:
-    print(clock.get_fps())
-    clock.tick()
+    clock.tick() # Starting game timer
     Note.render_background()
     Note.generate_timed_notes(pygame.time.get_ticks())
     Note.note_movement()
